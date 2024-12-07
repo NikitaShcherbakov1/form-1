0d057f1112bc8eebc95f8d5cef26ad9241734d59
NikitaShcherbakov1 <favour19900@gmail.com>
2024-12-07 18:09:46 +0400

    form-1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useRef, useState } from 'react';
+import { Field } from './components/field';
+import {
+	emailValidator,
+	passwordMinValidator,
+	passwordSymbolsValidator,
+} from './valodators';
+import styles from './app.module.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export const App = () => {
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+	const [passwordcheck, setPasswordcheck] = useState('');
 
-export default App;
+	const [isEmailValid, setIsEmailValid] = useState(false);
+	const [isPasswordValid, setIsPasswordValid] = useState(false);
+	const [isPasswordcheckValid, setIsPasswordcheckValid] = useState(false);
+
+	const submitButtonRef = useRef(null);
+
+	const onSubmit = (event) => {
+		event.preventDefault();
+		console.log({ email, password });
+	};
+
+	const isFormValid = isEmailValid && isPasswordValid && isPasswordcheckValid;
+
+	useEffect(() => {
+		if (isFormValid) {
+			submitButtonRef.current.focus();
+		}
+	}, [isFormValid]);
+
+	return (
+		<div className={styles.app}>
+			<form onSubmit={onSubmit}>
+				<Field
+					type="text"
+					name="email"
+					placeholder="Почта"
+					value={email}
+					setValue={setEmail}
+					setIsValid={setIsEmailValid}
+					validators={[emailValidator]}
+				/>
+				<Field
+					type="password"
+					name="password"
+					placeholder="Пароль"
+					value={password}
+					setValue={setPassword}
+					setIsValid={setIsPasswordValid}
+					validators={[passwordMinValidator, passwordSymbolsValidator]}
+				/>
+				<Field
+					type="password"
+					name="passwordcheck"
+					placeholder="Повтор пароля"
+					value={passwordcheck}
+					setValue={setPasswordcheck}
+					setIsValid={setIsPasswordcheckValid}
+					validators={[
+						(value) => (value === password ? null : 'Пароли не совпадают'),
+					]}
+					dependencies={{ password }}
+					forceValidation={(value) => value.length > 0}
+				/>
+				<button type="submit" disabled={!isFormValid} ref={submitButtonRef}>
+					Зарегистрироваться
+				</button>
+			</form>
+		</div>
+	);
+};
